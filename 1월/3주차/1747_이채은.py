394fb446fd943db255a4b917d85f6ffb558ed69d
lcheun <71924746+lcheun@users.noreply.github.com>
2022-01-18 03:56:42 +0900

    1747 소수&팰린드롬 이채은

---
+++ 1월/3주차/1747_이채은.py
@@ -0,0 +1,18 @@
+import math
+
+def isPrime(num):
+    for i in range(2, int(math.sqrt(num))):
+        if num % i == 0:
+            return False
+    return True
+
+def isPalindrome(num):
+    if str(num) == str(num)[::-1]:
+        return True
+    return False
+    
+N = int(input())
+for i in range(N, 2000001):
+    if isPalindrome(i) and isPrime(i) and i != 1:
+        print(i)
+        exit()
